db28800b70ec2cb4c30dcc2ffd7b3066cf89bd98
am-coding <amrikdeveloper@gmail.com>
2021-01-17 23:47:54 +0530

    added firebase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import AppContext from '../contexts/userContext'
+import firebase from '../firebase';
 
 export default function Page() {
+  useEffect(() => {
+    const fetchData =  () => {
+      const db = firebase.firestore()
+      const data = db.collection("headlines").doc('coding')
+      data.get().then(doc => {
+       console.log(doc.data());
+      })
+    }
+    fetchData();
+}, [])
+
   return (
     <div>
      hello
