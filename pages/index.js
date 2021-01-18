25a4e293858f641d6cad4e37f8f2d7145f0e5abf
am-coding <amrikdeveloper@gmail.com>
2021-01-18 16:21:39 +0530

    added articlesContext

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,15 @@
-import React, { useContext, useEffect } from 'react'
-import AppContext from '../contexts/userContext'
-import firebase from '../firebase';
+import React from 'react'
+import Articles from '../components/Articles';
+import { ArticleProvider } from '../components/articleWrapper'
 
 export default function Page() {
-  useEffect(() => {
-    const fetchData =  () => {
-      const db = firebase.firestore()
-      const data = db.collection("headlines").doc('coding')
-      data.get().then(doc => {
-       console.log(doc.data());
-      })
-    }
-    fetchData();
-}, [])
 
   return (
-    <div>
-     hello
-    </div>
+    <ArticleProvider>
+      <div>
+        <Articles />
+      </div>
+    </ArticleProvider>
+   
   )
 }
